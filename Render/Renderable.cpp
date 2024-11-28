77a4b96c99d5be64ecd7eb7d7cf5f991b25fc575
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-11-27 22:19:55 -0300

    feat: base definition for RbxStu::Render::Renderable

--- Render/Renderable.cpp
+++ Render/Renderable.cpp
@@ -4,7 +4,25 @@
 
 #include "Renderable.hpp"
 
-namespace RbxStu {
-namespace Render {
-} // Render
-} // RbxStu
+namespace RbxStu::Render {
+    void Renderable::DisableRender() {
+        this->m_bIsRenderingEnabled = false;
+    }
+
+    void Renderable::EnableRender() {
+        this->m_bIsRenderingEnabled = true;
+    }
+
+    bool Renderable::IsRenderingEnabled() {
+        return this->m_bIsRenderingEnabled;
+    }
+
+    void Renderable::Render(ImGuiContext *pContext) {
+    }
+
+    void Renderable::OnKeyPressed(ImmediateGui::VirtualKey key) {
+    }
+
+    void Renderable::OnKeyReleased(ImmediateGui::VirtualKey key) {
+    }
+}
--- Render/Renderable.hpp
+++ Render/Renderable.hpp
@@ -3,15 +3,41 @@
 //
 
 #pragma once
-#include "imgui_internal.h"
+#include <atomic>
+#include <imgui_internal.h>
+
 
 namespace RbxStu::Render {
+    namespace ImmediateGui {
+        enum class VirtualKey;
+    }
+
+    ///
+    /// @brief Provides an interface to define an object the ImGuiRendererJob can render into the screen utilizing dear ImGui.
+    ///
+    /// @remarks There are no windows created by default, only the ImGui frame is declared, you may create your own when overriding this class, however it is discouraged, and a Renderable object must only do simple things
+    /// unless it is a full-blown User Interface with only ImGui, in which it is justified, and you should look into RbxStu::Render::ImGui::* for utilities on writing such.
     class Renderable abstract {
+        std::atomic_bool m_bIsRenderingEnabled = false;
+
     public:
         virtual ~Renderable() = default;
 
-        virtual void Render(ImGuiContext *pContext);
+        void DisableRender();
 
-        virtual void OnKeyPressed();
+        void EnableRender();
+
+        bool IsRenderingEnabled();
+
+        /// @brief Ran for rendering by RbxStu's Rendering Hook.
+        /// @remarks The hook currently used for rendering is based on Kiero,
+        /// with D3D11 as its rendering API, do NOT use any other API to draw to the screen.
+        virtual void Render(ImGuiContext *pContext);;
+
+        /// @brief Ran by the RbxStu Window Procedure hook when a virtual key is pressed down.
+        virtual void OnKeyPressed(ImmediateGui::VirtualKey key);;
+
+        /// @brief Ran by the RbxStu Window Procedure hook when a virtual key is released.
+        virtual void OnKeyReleased(ImmediateGui::VirtualKey key);;
     };
 }
