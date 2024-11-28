30039b0319728eb4fe4fc1cfaa91454487ac77c3
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-11-28 11:35:06 -0300

    feat: refactor PagedWindow.xpp for better build performance.

--- Render/ImmediateGui/PagedWindow.hpp
+++ Render/ImmediateGui/PagedWindow.hpp
@@ -26,66 +26,16 @@ namespace RbxStu::Render::UI {
 
     public:
         explicit PagedWindow(const std::vector<UIPage> &pages,
-                             std::string szWindowName) {
-            this->m_pages = pages;
-            this->m_currentPageIndex = 0;
-            this->m_szWindowName = szWindowName;
-        }
+                             const std::string &szWindowName);
 
-        ~PagedWindow() {
-            this->m_pages.clear();
-        }
+        ~PagedWindow() override;
 
-        const UIPage &GetCurrentPage() {
-            return this->m_pages[this->m_currentPageIndex];
-        }
+        const UIPage &GetCurrentPage();
 
-        void SetCurrentPage(const int newCurrentPage) {
-            if (m_pages.size() > newCurrentPage) {
-                RbxStuLog(RbxStu::LogType::Warning, RbxStu::Graphics,
-                          "PagedWindow::SetCurrentPage(): Attempted to set the current page into index outside of the available page set, request dropped");
-                return;
-            }
+        void SetCurrentPage(const int newCurrentPage);
 
-            this->m_currentPageIndex = newCurrentPage;
-        }
+        void RenderPageButtons();
 
-        void RenderPageButtons() {
-            for (int i = 0; i < m_pages.size(); i++) {
-                const auto &page = m_pages[i];
-
-                if (this->m_currentPageIndex == i) {
-                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
-                    ImGui::Button(page.szPageName.c_str(), ImVec2(90, 30));
-                    ImGui::PopStyleColor();
-                } else {
-                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.19f, 0.19f, 0.19f, 0.54f));
-                    if (ImGui::Button(page.szPageName.c_str(), ImVec2(90, 30)))
-                        this->m_currentPageIndex = i;
-
-                    ImGui::PopStyleColor();
-                }
-            }
-        }
-
-        void Render(ImGuiContext *pContext) override {
-            ImGui::Begin("RbxStu::Render::UI::PagedWindow", nullptr, ImGuiWindowFlags_NoTitleBar);
-
-            ImGui::Text(this->m_szWindowName.c_str());
-
-            ImGui::GetWindowDrawList()->AddLine(
-                ImVec2(ImGui::GetCursorScreenPos().x - 9999, ImGui::GetCursorScreenPos().y),
-                ImVec2(ImGui::GetCursorScreenPos().x + 9999, ImGui::GetCursorScreenPos().y),
-                ImGui::GetColorU32(ImGuiCol_Border));
-            ImGui::Dummy(ImVec2(0.f, 5.f));
-
-            this->RenderPageButtons();
-
-            this->m_pages.at(this->m_currentPageIndex).pageRenderer->Render(pContext);
-
-            ImGui::End();
-
-            Renderable::Render(pContext);
-        };
+        void Render(ImGuiContext *pContext) override;;
     };
 }
