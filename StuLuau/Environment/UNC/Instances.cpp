d831eb2797ef0f62bdb55c9e84e6b617e5553a81
Pixeluted <pixeluted@proton.me>
2024-11-29 04:22:29 +0100

    feat: holy yap

---
+++ StuLuau/Environment/UNC/Instances.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Pixeluted on 29/11/2024.
+//
+
+#include "Instances.hpp"
+
+namespace RbxStu {
+namespace StuLuau {
+namespace Environment {
+namespace UNC {
+} // UNC
+} // Environment
+} // StuLuau
+} // RbxStu
---
+++ StuLuau/Environment/UNC/Instances.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Pixeluted on 29/11/2024.
+//
+
+namespace RbxStu::StuLuau::Environment::UNC {
+
+
+class Instances {
+
+};
+
+} // UNC
+
