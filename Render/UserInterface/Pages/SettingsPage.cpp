c7e73e553d79332d7642a1b25236ace5ff693a2b
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-11-29 15:03:55 -0300

    feat: initial SettingsPage content.

--- Render/UserInterface/Pages/SettingsPage.cpp
+++ Render/UserInterface/Pages/SettingsPage.cpp
@@ -4,8 +4,21 @@
 
 #include "SettingsPage.hpp"
 
+#include "FastFlags.hpp"
+
 namespace RbxStu::Render::UI::Pages {
+
     void SettingsPage::Render(ImGuiContext *pContext) {
+        ImGui::Text(">> RbxStu Information");
+
+        if (RbxStu::FastFlags::SFlagRbxCrashKey.IsDefined())
+            ImGui::Text("Current RbxStu::RBXCRASH key: '%s' %s", RbxStu::FastFlags::SFlagRbxCrashKey.GetValue().c_str(),
+                        "(Default)");
+        else
+            ImGui::Text("Current RbxStu::RBXCRASH key: '%s'", RbxStu::FastFlags::SFlagRbxCrashKey.GetValue().c_str());
+
+        Renderable::PushSeparator();
+
         Renderable::Render(pContext);
     }
-}
+} // namespace RbxStu::Render::UI::Pages
