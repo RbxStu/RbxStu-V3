5b6eea98b016d9058987687e2c08629d518a7f0e
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-11-28 23:22:07 -0300

    feat: renderable stub.

---
+++ Render/RenderableStub.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Dottik on 28/11/2024.
+//
+
+#include "RenderableStub.hpp"
+
+namespace RbxStu {
+namespace Render {
+} // Render
+} // RbxStu
---
+++ Render/RenderableStub.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Dottik on 28/11/2024.
+//
+
+#pragma once
+#include "Renderable.hpp"
+
+namespace RbxStu::Render {
+    class RenderableStub final : public RbxStu::Render::Renderable {
+    };
+}
