482cb3d9f2895320a313407cf27320920aeccde1
Pixeluted <pixeluted@proton.me>
2024-12-04 15:36:38 +0100

    fix: Fixed Bytecode functions Roblox modulescripts

--- Roblox/Script.cpp
+++ Roblox/Script.cpp
@@ -4,13 +4,14 @@
 
 #include "Script.hpp"
 
+#include <Logger.hpp>
 #include <Scheduling/TaskScheduler.hpp>
 #include <Scheduling/TaskSchedulerOrchestrator.hpp>
 
-#include "Luau/Compiler.h"
-#include "TypeDefinitions.hpp"
 #include <Windows.h>
 #include <string>
+#include "Luau/Compiler.h"
+#include "TypeDefinitions.hpp"
 
 namespace RbxStu::Roblox {
     Script::Script(void *pNative, const RbxStu::Roblox::Script::ScriptKind kind) : OpaqueClass(pNative) {
@@ -36,7 +37,8 @@ namespace RbxStu::Roblox {
         // Source access, required in ONLY in non-Team Create environments.
 
         const auto protectedString =
-                *reinterpret_cast<uintptr_t *>(reinterpret_cast<uintptr_t>(this->GetRealStructure()) + 0x1B8);
+                *reinterpret_cast<uintptr_t *>(reinterpret_cast<uintptr_t>(this->GetRealStructure()) +
+                                               (this->m_scriptKind == ScriptKind::ModuleScript ? 0x168 : 0x1B8));
         if (!protectedString || IsBadReadPtr(reinterpret_cast<void *>(protectedString), 0x10))
             return std::nullopt;
 
@@ -56,8 +58,8 @@ namespace RbxStu::Roblox {
                     /*
                      *  Bytecode offset: 0x1C0
                      */
-                    const auto protectedString = *reinterpret_cast<void **>(
-                            reinterpret_cast<uintptr_t>(this->GetRealStructure()) + 0x1C0);
+                    const auto protectedString =
+                            *reinterpret_cast<void **>(reinterpret_cast<uintptr_t>(this->GetRealStructure()) + 0x1C0);
                     if (!protectedString || IsBadReadPtr(protectedString, 0x10))
                         return std::nullopt;
 
@@ -67,8 +69,8 @@ namespace RbxStu::Roblox {
                     /*
                      *  Bytecode offset: 0x168
                      */
-                    const auto protectedString = *reinterpret_cast<void **>(
-                            reinterpret_cast<std::uintptr_t>(this->GetRealStructure()) + 0x168);
+                    const auto protectedString =
+                            *reinterpret_cast<void **>(reinterpret_cast<uintptr_t>(this->GetRealStructure()) + 0x168);
                     if (!protectedString || IsBadReadPtr(protectedString, 0x10))
                         return std::nullopt;
 
@@ -85,6 +87,12 @@ namespace RbxStu::Roblox {
         const auto src = this->GetSource();
         if (!src.has_value())
             return std::nullopt;
+
+        // We need to check if it is a roblox script module script, bc if yes, then the source is bytecode
+        if (this->m_scriptKind == ScriptKind::ModuleScript &&
+            *reinterpret_cast<int32_t *>(reinterpret_cast<uintptr_t>(this->GetRealStructure()) + 0x1B0) == 1) {
+            return src.value();
+        }
 
         auto opts = Luau::CompileOptions{};
         opts.debugLevel = 2;
