c6d97a045aebd5a60cd7ce77f061375de9d5fbeb
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2025-01-01 15:40:11 -0300

    feat: define new luau extension macros.

--- StuLuau/Extensions/luauext.hpp
+++ StuLuau/Extensions/luauext.hpp
@@ -5,8 +5,13 @@
 
 #include <lua.h>
 #include <lualib.h>
+#include <lgc.h>
 
 #define lua_normalisestack(L, maxSize) { if (lua_gettop(L) > maxSize) lua_settop(L, maxSize); }
+
+#define lua_preparepush(L, pushCount) lua_rawcheckstack(L, pushCount)
+
+#define lua_preparepushcollectable(L, pushCount) { lua_preparepush(L, pushCount); luaC_threadbarrier(L); }
 
 #define lua_enforcestack(L, maxSize) { if (lua_gettop(L) > maxSize) luaL_errorL(L, "function expects only %d arguments, you provided %d.", maxSize, lua_gettop(L)); }
 
